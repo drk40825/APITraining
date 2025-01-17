d14770ef8557fc2b377631b6caf3900e1fb51044
Deepak Kashyap <drk40825@gmail.com>
2025-01-17 03:39:34 -0800

    oAuth Server

---
+++ src/main/java/com/oauth2server/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.oauth2server.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+            	.requestMatchers("/", "/login", "/oauth2/**").permitAll()
+                .anyRequest().authenticated()
+            )
+           // .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/emplist", true));
+ 
+          .oauth2Login();
+       // .oauth2Login(oauth2 -> oauth2
+        //        .loginPage("/login.html")
+        //    );
+        return http.build();
+    }
+}
